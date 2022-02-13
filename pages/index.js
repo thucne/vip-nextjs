8846512408354400eda2f02c6df84f8ee108dfa0
Trong Thuc - 162864 <trongthuc.bentre@gmail.com>
2022-02-13 19:54:47 +0700

    ud

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,12 @@
+import Meta from "@components/Meta";
+import Navigatiopn from '@components/Navigation';
+
 export default function Home() {
   return (
-    <h1 className="text-3xl font-bold underline">
-      Hello world!
-    </h1>
-  )
+    <div>
+      <Meta />
+      <Navigatiopn />
+      <h1 className="text-3xl font-bold underline">Hello world!</h1>
+    </div>
+  );
 }
