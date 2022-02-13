419495f60bdff2cc2ab4fc942e1feb9e1b00fb40
KhangDuy <nguyenduykhangcd12t4@gmail.com>
2022-02-13 21:42:16 +0700

    capitalize error

--- components/dashboard/WordsVerticalList.jsx
+++ components/dashboard/WordsVerticalList.jsx
@@ -9,12 +9,12 @@ import NewWords from './NewWords';
  const WordsVerticalList = (props) => {
   return(
     <div className='flex flex-col h-full ml-10'>
-        <div className='flex basis-1/2'>
+        <div className='flex w-full basis-1/2'>
             <NewWords/>
         </div>
 
         <div className='flex basis-1/2'>
-            hello
+            Lorem ipsum dolor, sit amet consectetur adipisicing elit. Reiciendis minus perferendis officiis ad laudantium voluptatibus eum, sint earum veniam numquam maiores autem? Autem, a atque. Placeat vero ullam soluta odio.
         </div>
     </div>
    )
--- components/dashboard/index.jsx
+++ components/dashboard/index.jsx
@@ -8,12 +8,12 @@ import WordsVerticalList from "./WordsVerticalList";
 
 const Dashboard = (props) => {
   return (
-    <div className="flex flex-col md:flex-row p-10 h-screen">
+    <div className="flex flex-col md:flex-row p-3 md:p-10 h-screen">
       <div className="flex flex-col w-full md:basis-1/2 md:border-r-[1px] md:border-r-gray-300 h-full">
             <WordsVerticalList/>
       </div>
 
-      <div className="flex flex-colw-full  md:basis-1/2 h-full">
+      <div className="flex flex-col w-full  md:basis-1/2 h-full">
           
       </div>
     </div>
